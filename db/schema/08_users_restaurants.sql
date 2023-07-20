658f1bede341fc3d2fb414c431050cc0168a4d72
Natalia M <natalia.martian@gmail.com>
2023-07-20 17:46:59 -0400

    Update schemas + seeds to include is_done boolean (preparation for Done workflow)

--- db/schema/08_users_restaurants.sql
+++ db/schema/08_users_restaurants.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS users_restaurants CASCADE;
 CREATE TABLE users_restaurants (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE
+  restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
+  is_done BOOLEAN NOT NULL
 );
--- db/seeds/04_user_category_seeds.sql
+++ db/seeds/04_user_category_seeds.sql
@@ -1,63 +1,63 @@
 -- users_movies seeds
-INSERT INTO users_movies (user_id, movie_id)
+INSERT INTO users_movies (user_id, movie_id, is_done)
 VALUES
-  (1, 1),
-  (1, 3),
-  (2, 2),
-  (2, 1),
-  (3, 3),
-  (3, 2),
-  (4, 1),
-  (4, 3),
-  (5, 3),
-  (5, 2),
-  (6, 2),
-  (6, 1);
+  (1, 1, TRUE),
+  (1, 3, FALSE),
+  (2, 2, TRUE),
+  (2, 1, FALSE),
+  (3, 3, TRUE),
+  (3, 2, FALSE),
+  (4, 1, TRUE),
+  (4, 3, FALSE),
+  (5, 3, TRUE),
+  (5, 2, FALSE),
+  (6, 2, TRUE),
+  (6, 1, FALSE);
 
 -- users_restaurants seeds
-INSERT INTO users_restaurants (user_id, restaurant_id)
+INSERT INTO users_restaurants (user_id, restaurant_id, is_done)
 VALUES
-  (1, 2),
-  (1, 3),
-  (2, 4),
-  (2, 1),
-  (3, 1),
-  (3, 3),
-  (4, 4),
-  (4, 3),
-  (5, 1),
-  (5, 2),
-  (6, 2),
-  (6, 4);
+  (1, 2, FALSE),
+  (1, 3, TRUE),
+  (2, 4, FALSE),
+  (2, 1, TRUE),
+  (3, 1, FALSE),
+  (3, 3, TRUE),
+  (4, 4, FALSE),
+  (4, 3, TRUE),
+  (5, 1, FALSE),
+  (5, 2, TRUE),
+  (6, 2, FALSE),
+  (6, 4, TRUE);
 
 -- users_books seeds
-INSERT INTO users_books (user_id, book_id)
+INSERT INTO users_books (user_id, book_id, is_done)
 VALUES
-  (1, 2),
-  (1, 3),
-  (2, 2),
-  (2, 1),
-  (3, 1),
-  (3, 3),
-  (4, 2),
-  (4, 3),
-  (5, 3),
-  (5, 1),
-  (6, 2),
-  (6, 1);
+  (1, 2, TRUE),
+  (1, 3, FALSE),
+  (2, 2, TRUE),
+  (2, 1, FALSE),
+  (3, 1, TRUE),
+  (3, 3, FALSE),
+  (4, 2, TRUE),
+  (4, 3, FALSE),
+  (5, 3, TRUE),
+  (5, 1, FALSE),
+  (6, 2, TRUE),
+  (6, 1, FALSE);
 
   -- users_products seeds
-INSERT INTO users_products (user_id, product_id)
+INSERT INTO users_products (user_id, product_id, is_done)
 VALUES
-  (1, 2),
-  (1, 3),
-  (2, 2),
-  (2, 1),
-  (3, 1),
-  (3, 3),
-  (4, 2),
-  (4, 3),
-  (5, 3),
-  (5, 1),
-  (6, 2),
-  (6, 1);
+  (1, 2, TRUE),
+  (1, 3, FALSE),
+  (2, 2, TRUE),
+  (2, 1, FALSE),
+  (3, 1, TRUE),
+  (3, 3, FALSE),
+  (4, 2, TRUE),
+  (4, 3, FALSE),
+  (5, 3, TRUE),
+  (5, 1, FALSE),
+  (6, 2, TRUE),
+  (6, 1, FALSE);
